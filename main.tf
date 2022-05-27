55b7ef1a450253a308c5c0f2580508c20ebd2de9
Gevorg Amirjanyan <gevorg.amirjanyan@volo.global>
2022-05-27 18:57:30 +0400

    + database

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 resource "azurerm_resource_group" "example" {
   name     = "MyTerraformRG"
-  location = "West Europe"
+  location = "Central US"
 }
 
 resource "azurerm_virtual_machine" "main" {
@@ -50,4 +50,33 @@ resource "azurerm_virtual_machine_extension" "example" {
   tags = {
     environment = "Production"
   }
+}
+
+resource "azurerm_mysql_server" "example" {
+  name                = "example-mysqlserver7778"
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+
+  administrator_login          = "mysqladminun"
+  administrator_login_password = "H@Sh1CoR3!"
+
+  sku_name   = "B_Gen5_2"
+  storage_mb = 5120
+  version    = "5.7"
+
+  auto_grow_enabled                 = true
+  backup_retention_days             = 7
+  geo_redundant_backup_enabled      = false
+  infrastructure_encryption_enabled = false
+  public_network_access_enabled     = true
+  ssl_enforcement_enabled           = true
+  ssl_minimal_tls_version_enforced  = "TLS1_2"
+}
+
+resource "azurerm_mysql_database" "example" {
+  name                = "exampledb"
+  resource_group_name = azurerm_resource_group.example.name
+  server_name         = azurerm_mysql_server.example.name
+  charset             = "utf8"
+  collation           = "utf8_unicode_ci"
 }
